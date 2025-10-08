2aada9589a6fc10334f1f8d44e78572d338ed3f5
Usama Ismail <kingusamakhan@gmail.com>
2025-10-08 21:58:58 +0500

    Add Participant and Event controller

---
+++ Event-Scheduler.Api/Controllers/ParticipantController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Scheduler.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipantController : ControllerBase
+    {
+    }
+}
