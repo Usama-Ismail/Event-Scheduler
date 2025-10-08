2e364d3e79a02d2e1809a5580da5d06d4adf8476
Usama Ismail <kingusamakhan@gmail.com>
2025-10-08 21:49:22 +0500

    Add Event and Participant model

---
+++ Event-Scheduler.Api/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿namespace Event_Scheduler.Api.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public virtual ICollection<Participant> Participants { get; set; } = [];
+
+    }
+}
